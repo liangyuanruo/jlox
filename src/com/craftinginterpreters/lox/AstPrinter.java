1cb263b942cd6352b9b5fc43c40a1b361933a157
Yuanruo Liang <yuanruo@open.gov.sg>
2022-01-23 11:17:33 +0800

    feat(Assignment): allow assignments in the global scope

--- src/com/craftinginterpreters/lox/AstPrinter.java
+++ src/com/craftinginterpreters/lox/AstPrinter.java
@@ -29,6 +29,11 @@ public class AstPrinter implements Expr.Visitor<String> {
     @Override
     public String visitVariableExpr(Expr.Variable expr) { return expr.name.lexeme; }
 
+    @Override
+    public String visitAssignExpr(Expr.Assign expr) {
+        return parenthesize("= " + expr.name.lexeme + " " + expr.accept(this));
+    }
+
     private String parenthesize(String name, Expr... exprs) {
         StringBuilder builder = new StringBuilder();
 
