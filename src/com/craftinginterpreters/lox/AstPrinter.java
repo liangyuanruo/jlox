51817787256f616f0d605353b5f8fc63a0e18ef4
Yuanruo Liang <yuanruo@open.gov.sg>
2022-01-30 16:38:44 +0800

    feat(Logical): implement logical operators (and, or)

--- src/com/craftinginterpreters/lox/AstPrinter.java
+++ src/com/craftinginterpreters/lox/AstPrinter.java
@@ -34,6 +34,11 @@ public class AstPrinter implements Expr.Visitor<String> {
         return parenthesize("= " + expr.name.lexeme + " " + expr.accept(this));
     }
 
+    @Override
+    public String visitLogicalExpr(Expr.Logical expr) {
+        return parenthesize(expr.operator.lexeme + " " + expr.left + " " + expr.right);
+    }
+
     private String parenthesize(String name, Expr... exprs) {
         StringBuilder builder = new StringBuilder();
 
